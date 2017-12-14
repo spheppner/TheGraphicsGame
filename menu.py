ad4cf333bb625e92bec341b7ea7cb078cb5a38fa
Simon Heppner <hasibandi@gmx.at>
2017-12-14 17:21:53 +0100

    Update menu.py

--- menu.py
+++ menu.py
@@ -5,7 +5,6 @@ menu system for pygame
 
 
 import pygame 
-#import template004_sprites_collision_detection
 import textscroller_vertical
 import random
 import sys
@@ -13,21 +12,10 @@ import os.path
 
 
 class Settings(object):
-    gold = 100
-    red_defenders = 0
-    red_attackers = 0
-    blue_defenders = 0
-    blue_attackers = 0
-    price_red = 10
-    price_blue = 10
-    menu = {"root":["Play","buy red army", "buy blue army", "Difficulty", "Help", "Credits", "Options","Quit"],
-            "buy blue army": ["buy blue defender", "buy blue attacker", "sell blue defender", "sell blue attacker"],
-            "buy red army":  ["buy red defender", "buy red attacker", "sell red defender", "sell red attacker"],
-            "Options":["Turn music off","Turn sound off","Change screen resolution", "Turn off Wilhelm"],
-            "Difficulty":["easy","medium","elite","hardcore"],
+    menu = {"root":["Play", "Help", "Credits", "Options","Quit"],
+            "Options":["Turn music off","Turn sound off","Change screen resolution"],
             "Change screen resolution":["640x400","800x640","1024x800"],
-            "Credits":["bla1","bla2", "False"],
-            "Help":["how to play", "how to win"]
+            "Credits":["Simon HEPPNER","Horst JENS"],
             } 
         
 
@@ -99,12 +87,7 @@ class PygView(object):
         pygame.mixer.pre_init(44100, -16, 2, 2048) 
 
         pygame.init()
-        
-        #self.cash = pygame.mixer.Sound(os.path.join("data","cash.wav"))
-        #jump = pygame.mixer.Sound(os.path.join('data','jump.wav'))  #load sound
-        #self.sound1 = pygame.mixer.Sound(os.path.join('data','Pickup_Coin.wav'))
-        #self.sound2 = pygame.mixer.Sound(os.path.join('data','Jump.wav'))
-        #self.sound3 = pygame.mixer.Sound(os.path.join('data','mix.wav'))
+
         pygame.display.set_caption("Press ESC to quit")
         PygView.width = width
         PygView.height = height
@@ -128,11 +111,6 @@ class PygView(object):
                 self.draw_text(i, 100, m.items.index(i)*30+10,(0,0,255))
             else:
                 self.draw_text(i, 100, m.items.index(i)*30+10)
-        
-        y = self.height - 120
-        self.draw_text("Gold: {}".format(Settings.gold), 10,y , (200,200,0))
-        self.draw_text("Red: A:{} D:{}".format(Settings.red_attackers, Settings.red_defenders), 150, y, (200,0,0))
-        self.draw_text("Blue: A:{} D:{}".format(Settings.blue_attackers, Settings.blue_defenders), 340, y, (0,0,200))
 
     def run(self):
         """The mainloop
@@ -147,17 +125,12 @@ class PygView(object):
                     if event.key == pygame.K_ESCAPE:
                         running = False
                     if event.key==pygame.K_DOWN or event.key == pygame.K_KP2:
-                        #print(m.active_itemnumber)
                         m.nextitem()
                         print(m.active_itemnumber)
-                        #self.sound2.play()
                     if event.key==pygame.K_UP or event.key == pygame.K_KP8:
                         m.previousitem()
-                        #self.sound1.play()
                     if event.key==pygame.K_RETURN or event.key == pygame.K_KP_ENTER:
-                        #self.sound3.play()
                         result = m.get_text()
-                        #print(m.get_text())
                         print(result)
                         if result is None:
                             break 
@@ -173,92 +146,26 @@ class PygView(object):
                         
                         # important: no elif here, instead if, because every menupoint could contain an 'x'        
                         elif result=="Play":
-                            # simpledefense.PygView().run()
                             print("activating external program")
-                            #externalProgram.PygView(self.width, self.height)
                             pygame.quit()
-                            template004_sprites_collision_detection.PygView(900,900).run()
-                            #sys.exit()    
+                            # START GAME  
                             print("bye") 
                             self.__init__()
-                                                  
-                        elif result == "buy blue defender":
-                            self.cash.play()
-                            if Settings.gold >= Settings.price_blue:
-                                Settings.gold -= Settings.price_blue
-                                Settings.blue_defenders += 1
-                            else:
-                                print("earn some money first")
-                        elif result == "buy red defender":
-                            self.cash.play()
-                            if Settings.gold >= Settings.price_red:
-                                Settings.gold -= Settings.price_red
-                                Settings.red_defenders += 1
-                            else:
-                                print("earn some money first")
-                        elif result == "buy blue attacker":
-                            self.cash.play()
-                            if Settings.gold >= Settings.price_blue:
-                                Settings.gold -= Settings.price_blue
-                                Settings.blue_attackers += 1
-                            else:
-                                print("earn some money first")
-                        elif result == "buy red attacker":
-                            self.cash.play()
-                            if Settings.gold >= Settings.price_red:
-                                Settings.gold -= Settings.price_red
-                                Settings.red_attackers += 1
-                            else:
-                                print("earn some money first")
-                                           
-                        #------ sell ------
-                        elif result == "sell blue defender":
-                            self.cash.play()
-                            if Settings.blue_defenders > 0:
-                                Settings.gold += Settings.price_blue / 2
-                                Settings.blue_defenders -= 1
-                            else:
-                                print("you have no blue defenders")
-                        elif result == "sell red defender":
-                            self.cash.play()
-                            if Settings.red_defenders > 0:
-                                Settings.gold += Settings.price_red / 2
-                                Settings.red_defenders -= 1
-                            else:
-                                print("you have no red defenders")
-                        elif result == "sell blue attacker":
-                            self.cash.play()
-                            if Settings.blue_attackers > 0:
-                                Settings.gold += Settings.price_blue / 2
-                                Settings.blue_attackers -= 1
-                            else:
-                                print("you have no blue attackers")
-                        elif result == "sell red attacker":
-                            self.cash.play()
-                            if Settings.red_attackers > 0:
-                                Settings.gold += Settings.price_red / 2
-                                Settings.red_attackers -= 1
-                            else:
-                                print("you have no red attackers")
-                                           
-                    
-                    
-                    
-                    
-                                
-                        elif result == "how to play":
-                            text="play this game\n as you like\n and win!"
+                        elif result == "Help":
+                            text="You need to\ncontrol a ship\nand make graphics\nwhile the other, automated\nplayer flies around!"
                             textscroller_vertical.PygView(text, self.width, self.height).run()
-                        elif result == "nix":
-                            text="nix\n gar nix\n wirklich nix!"
-                            textscroller_vertical.PygView(text, self.width, self.height).run()
-                        elif result == "how to win":
-                            text="to win the game:\n shoot down enemies\n avoid catching bullets"
-                            textscroller_vertical.PygView(text, self.width, self.height, bg_filename=os.path.join("data", "800px-La_naissance_de_Venus.jpg")).run()
-                        elif result == "False":
-                            Settings.menu["Credits"][2] = "True" # toggle
-                        elif result == "True":
-                           Settings.menu["Credits"][2] = "False" # toggle
+                        elif result == "Turn music off":
+							#TURN MUSIC OFF
+                            Settings.menu["Options"][0] = "Turn music on"
+                        elif result == "Turn music on":
+							#TURN MUSIC ON
+                            Settings.menu["Options"][0] = "Turn music off"
+                        elif result == "Turn sound off":
+							#TURN MUSIC OFF
+                            Settings.menu["Options"][1] = "Turn sound on"
+                        elif result == "Turn sound on":
+							#TURN MUSIC ON
+                            Settings.menu["Options"][1] = "Turn sound off"
                         elif result=="Quit":
                             print("Bye")
                             pygame.quit()
